5f64bfb560dbc1a5f846d0ac0e913e7f4b12e9ba
DmitrySergienko <dmitrisergienko@gmail.com>
2022-08-31 17:04:46 +0400

    Recycler attached

--- app/src/main/java/com/example/fitnessconstructor/ui/workout/WorkoutFragment.kt
+++ app/src/main/java/com/example/fitnessconstructor/ui/workout/WorkoutFragment.kt
@@ -1,7 +1,10 @@
 package com.example.fitnessconstructor.ui.workout
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.fitnessconstructor.databinding.FragmentWorkoutBinding
 import com.example.fitnessconstructor.ui.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,6 +14,25 @@ class WorkoutFragment : BaseFragment<FragmentWorkoutBinding>(FragmentWorkoutBind
 
     private val viewModel: WorkoutViewModel by viewModels()
     private val args: WorkoutFragmentArgs by navArgs()
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //initRecyclerView()
+
+    }
+
+    fun initRecyclerView(){
+        var todoList = TODO("Add list of items for recycler")
+        val adapter = WorkoutAdapter(todoList,requireContext())
+        binding.apply {
+            recyclerWorkout.adapter = adapter
+            recyclerWorkout.layoutManager = LinearLayoutManager(requireContext())
+
+        }
+    }
+
 
     companion object {
 
