b7154300897e596a7d3ac9d365e0b6dcbeca5387
DmitrySergienko <dmitrisergienko@gmail.com>
2022-08-31 16:21:46 +0400

    Recycler attached

---
+++ app/src/main/java/com/example/fitnessconstructor/ui/workoutlist/WorkoutListAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.fitnessconstructor.ui.workoutlist
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fitnessconstructor.R
+import com.example.fitnessconstructor.databinding.ItemWorkoutListBinding
+import com.example.fitnessconstructor.domain.Workout
+
+class WorkoutListAdapter(
+    todo: List<Workout>, // Workout!??
+    context: Context
+) : RecyclerView.Adapter<WorkoutListAdapter.WorkoutListHolder>() {
+
+    private val sectorItemList = todo
+    private val contextAdapter = context
+
+    class WorkoutListHolder(item: View) : RecyclerView.ViewHolder(item) {
+        val binding = ItemWorkoutListBinding.bind(item)
+
+        fun bind(item: Workout) = with(binding) {
+            //TODO bind elements with item
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WorkoutListHolder {
+        val view =
+            LayoutInflater.from(contextAdapter).inflate(R.layout.item_workout_list, parent, false)
+        return WorkoutListHolder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: WorkoutListHolder, position: Int) {
+        holder.bind(sectorItemList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return sectorItemList.size
+    }
+
+}
