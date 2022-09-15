761d1610616ef3d50cdc122dfac6b9116caf1a15
DT5Gen <marinovod@gmail.com>
2022-09-16 02:06:59 +0300

    added gifs

---
+++ app/src/main/java/com/example/fitnessconstructor/utils/TimeUtils.kt
@@ -0,0 +1,16 @@
+package com.example.fitnessconstructor.utils
+
+import android.annotation.SuppressLint
+import java.text.SimpleDateFormat
+import java.util.*
+
+object TimeUtils {
+    @SuppressLint("SimpleDateFormat")
+    val formater = SimpleDateFormat("mm:ss")
+
+    fun getTime(time: Long) : String{
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = time
+        return formater.format(calendar.time)
+    }
+}
