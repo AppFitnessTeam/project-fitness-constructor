fb2e1db3c7f83ba7be970ea4febb479eb44210e1
Aleksei Laptev <mar.d.soul.app@gmail.com>
2022-09-29 13:09:42 +0300

    add view model and data store

--- app/src/main/java/com/example/fitnessconstructor/ui/usersettings/UserSettingsFragment.kt
+++ app/src/main/java/com/example/fitnessconstructor/ui/usersettings/UserSettingsFragment.kt
@@ -2,6 +2,7 @@ package com.example.fitnessconstructor.ui.usersettings
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.example.fitnessconstructor.databinding.FragmentUserSettingsBinding
 import com.example.fitnessconstructor.ui.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -9,6 +10,8 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class UserSettingsFragment :
     BaseFragment<FragmentUserSettingsBinding>(FragmentUserSettingsBinding::inflate) {
+
+    private val viewModel:UserSettingsViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
